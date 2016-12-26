076a7c0ede06b33f7461e33078223412a3c9ded3
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-12-26 22:08:13 +0700

    - Create CollectionView and make paging. - Adding `page controls`.

--- SlideViewWithCollectionViewVer2/ViewController.swift
+++ SlideViewWithCollectionViewVer2/ViewController.swift
@@ -12,9 +12,43 @@ class ViewController: UIViewController {
     // MARK: - IBOutlet
     @IBOutlet weak var collectionView: UICollectionView!
     
+    // MARK: - Variable
+    let CVCellIdentifier = "CollectionViewCell"
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 }
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CVCellIdentifier,
+                                                      for: indexPath)
+        if indexPath.row%2 == 0 {
+            cell.backgroundColor = UIColor.blue
+        } else {
+            cell.backgroundColor = UIColor.gray
+        }
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let w: CGFloat = (collectionView.frame.size.width)
+        let h: CGFloat = (collectionView.frame.size.height) - 64
+        return CGSize(width: w, height: h)
+    }
+}
+
 
