35a5c25e72feb2893b169835eb31cd9207e17819
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-12-26 21:44:12 +0700

    - Initial Project.

---
+++ SlideViewWithCollectionViewVer2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideViewWithCollectionViewVer2
+//
+//  Created by Kittitat Rodphotong on 12/26/2559 BE.
+//  Copyright © 2559 DevGo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - IBOutlet
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
